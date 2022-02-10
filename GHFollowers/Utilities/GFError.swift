5fdb1416b62abef6f488f6d7035b8c12665d9fcd
Oo-Fantomas-oO <777sirj777@gmail.com>
2022-02-10 16:49:24 +0200

    Persistence manager, UITableView FavoritesVC.

--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -8,8 +8,10 @@
 import Foundation
 
 enum GFError: String, Error {
-    case ivalidUsername   = "This username created an invalid request. Please try again."
-    case unableToComplied = "Unable to complete your request. Please check your internet connection."
-    case invalidResponse  = "Invalid response from the server. Please try again."
-    case invalidData      = "The data received from the server was invalid. Please try again."
+    case ivalidUsername     = "This username created an invalid request. Please try again."
+    case unableToComplied   = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse    = "Invalid response from the server. Please try again."
+    case invalidData        = "The data received from the server was invalid. Please try again."
+    case unableToFavorite   = "There was an error favoriting this user. Please try again."
+    case alreadyInFavorites = "You've already favorited this user. You are REALLY like them!"
 }
